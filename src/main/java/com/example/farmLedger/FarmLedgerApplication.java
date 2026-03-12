587ef39514b34c3f6071fb1b76ddcf50dd6205b7
shans94 <sunilsam.sunil@gmail.com>
2026-03-12 22:41:55 +0530

    Initial commit

---
+++ src/main/java/com/example/farmLedger/FarmLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmLedger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/farmLedger/FarmLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
